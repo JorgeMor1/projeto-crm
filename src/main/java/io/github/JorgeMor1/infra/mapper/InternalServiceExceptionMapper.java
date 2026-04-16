38b3f4c16b2e07fb8a52e82b7ea625897e99c70a
Jorge Luiz <batistajoorge@gmail.com>
2026-04-15 21:57:36 -0300

    refactor(mapper): Criando um mapper para o Internal Error e tratando o erro 500 para trazer uma mensagem mais haminizada.

--- src/main/java/io/github/JorgeMor1/infra/mapper/InternalServiceExceptionMapper.java
+++ src/main/java/io/github/JorgeMor1/infra/mapper/InternalServiceExceptionMapper.java
@@ -1,4 +1,34 @@
 package io.github.JorgeMor1.infra.mapper;
 
-public class InternalServiceExceptionMapper {
+import io.github.JorgeMor1.dto.ErrorResponse;
+import io.github.JorgeMor1.exception.BusinessException;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+import org.jboss.logging.Logger;
+
+@Provider
+public class InternalServiceExceptionMapper implements ExceptionMapper<Exception> {
+
+    private static final Logger LOG = Logger.getLogger(InternalServiceExceptionMapper.class);
+
+    @Context
+    UriInfo uriInfo;
+    @Override
+    public Response toResponse(Exception exception){
+
+        LOG.error("Erro inesperado na aplicação", exception);
+
+        ErrorResponse error = new ErrorResponse(
+                500,
+                "Internal Server Error",
+                "Ocorreu um erro inesperado",
+                uriInfo.getPath()
+        );
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(error)
+                .build();
+    }
 }
